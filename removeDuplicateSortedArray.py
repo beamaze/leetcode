86253bc52d8244fbbb4d5957a36984d28d2086b8
beamaze <ptokhem@ucmerced.edu>
2021-01-29 01:07:56 -0800

    Revert "Create removeDuplicateSortedArray.py"
    This reverts commit 512e6d26cea240264c048063ed4ebea0c38645a3.

--- removeDuplicateSortedArray.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        count = 0
-        for i in range(1, len(nums)):
-            if nums[count] < nums[i]:
-                count += 1
-                nums[count] = nums[i]
-        return count + 1
